6f3d3ccf20dd4ef665d125991926f25b58173eb1
hungnq10 <nguyenhungpes18@gmail.com>
2025-06-11 23:04:39 +0700

    add lop hoc ao

---
+++ frontend/src/pages/Tour360.jsx
@@ -0,0 +1,18 @@
+
+function Tour360() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
+      <iframe
+        width="960"
+        height="480"
+        src="https://www.thinglink.com/view/scene/1989244403985679204"
+        title="360 Virtual Tour"
+        style={{ border: 'none' }}
+        allowFullScreen
+        scrolling="no"
+      ></iframe>
+    </div>
+  );
+}
+
+export default Tour360;
