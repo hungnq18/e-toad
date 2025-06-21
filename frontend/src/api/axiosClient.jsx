ba0e8388ac70c1a635b3589c9798bdd70cf29353
hungnq10 <nguyenhungpes18@gmail.com>
2025-06-22 00:49:10 +0700

    cấu hình deploy

--- frontend/src/api/axiosClient.jsx
+++ frontend/src/api/axiosClient.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://e-toad.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
