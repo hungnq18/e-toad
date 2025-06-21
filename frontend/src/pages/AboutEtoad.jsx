6659c014797f99915da3a9ae966e7cc71cd91eee
hungnq10 <nguyenhungpes18@gmail.com>
2025-06-22 01:19:22 +0700

    aboutetoad tiếp

--- frontend/src/pages/AboutEtoad.jsx
+++ frontend/src/pages/AboutEtoad.jsx
@@ -1,10 +1,10 @@
-
 import Abe1 from '../component/Abe1';
 import Abe2 from '../component/Abe2';
 import Abe3 from '../component/Abe3';
 import Abe4 from '../component/Abe4';
 import StorySection from '../component/bannerabe';
-import "../component/css/aboutEToad.css";
+import "../component/css/aboutEtoad.css";
+
 function AboutEtoad() {
   return (
 <div>
