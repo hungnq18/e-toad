d20cbef6f01a398cc0a9b6acefec5bd7b4babe2f
hungnq10 <nguyenhungpes18@gmail.com>
2025-06-23 08:04:17 +0700

    sửa lại tailwindcss

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
