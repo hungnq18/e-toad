7083bf8bfe6e27d8f2f8c8a8eb16b5b64126c5f9
hungnq10 <nguyenhungpes18@gmail.com>
2025-07-01 09:42:08 +0700

    thêm upload ảnh

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,9 +1,11 @@
 const cloudinary = require('cloudinary').v2;
 
+console.log('Cloudinary ENV:', process.env.CLOUDINARY_CLOUD_NAME, process.env.CLOUDINARY_API_KEY, process.env.CLOUDINARY_API_SECRET);
+
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: 'dijayprrw',
+  api_key: '519957917434212',
+  api_secret: 'podPs8YKmPeDvh5iCd5KiPqm9qE',
 });
 
 module.exports = cloudinary; 
